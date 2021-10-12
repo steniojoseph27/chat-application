5d503285768a9d1fb18a7c0426ee958d7f11d8ff
Stenio Joseph <49082249+steniojoseph27@users.noreply.github.com>
2021-10-11 23:59:20 -0400

    Fixing CSS

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+﻿const gulp = require("gulp");
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
+
+function minify() {
+    var js = gulp.src(["wwwroot/js/**/*.js"])
+        .pipe(uglify())
+        .pipe(concat("dutchtreat.min.js"))
+        .pipe(gulp.dest("wwwroot/dist/"));
+
+    return js;
+}
+
+function styles() {
+    var css = gulp.src(["wwwroot/css/**/*.css"])
+        .pipe(uglify())
+        .pipe(concat("chatapp.min.css"))
+        .pipe(gulp.dest("wwwroot/dist/"));
+
+    return css;
+}
+
+
+exports.styles = styles;
+exports.minify = minify;
+exports.default = gulp.parallel(styles, minify)
